acebdced7c936d68106925068e2d7ffef1e06890
Kamil Hubert Zalewski <kamil.hubert.zalewski@gmail.com>
2021-03-15 18:22:33 +0000

    Initial commit

---
+++ sql_solutions.sql
@@ -0,0 +1,33 @@
+-- SELECT * FROM movies;
+
+-- SELECT name FROM people;
+
+-- UPDATE people
+-- SET name = 'Krusty the Clown'
+-- WHERE name = 'Crusty the Clown';
+
+-- SELECT name FROM people
+-- WHERE name = 'Homer Simpson';
+
+-- DELETE FROM movies
+-- WHERE title = 'Batman Begins';
+
+-- INSERT INTO people (name)
+-- VALUES ('Bart Simpson');
+
+-- DELETE FROM people
+-- WHERE name = 'Eric Cartman';
+
+-- INSERT INTO movies (title)
+-- VALUES ('Avengers: Infinity War');
+
+
+-- UPDATE movies
+-- SET show_time = '23:55'
+-- WHERE title = 'Iron Man 3';
+
+SELECT * FROM people;
+
+-- DELETE from people
+-- WHERE name = 'Bart Simpson';
+
